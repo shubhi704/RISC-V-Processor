1de566d86bf5e250e94b659ecf44c3ee191cf487
shubhi704 <82434808+shubhi704@users.noreply.github.com>
2021-08-28 17:29:33 +0530

    Update regbank.v

--- regbank.v
+++ regbank.v
@@ -1,15 +1,18 @@
-  module regbank(RE_A, RE_B, RE_X, WD, MEM_WD, SRA, SRB, DR, W_INST, R_INST, CLK);
+module #(parameter ADDR_WIDTH = 6,
+                    parameter DATA_WIDTH = 32,
+                    parameter DEPTH = (2*32)-1)
+	  regbank  (RE_A, RE_B, RE_X, WD, MEM_WD, SRA, SRB, DR, W_INST, R_INST, CLK);
     
 	 input CLK;
-	 input [5:0] SRA, SRB,W_INST,R_INST, DR;   //source and destination reg
-	 input [31:0] WD, MEM_WD;            // write data
-	 output [31:0] RE_X,RE_A, RE_B;    //read data 
+	input [ADDR_WIDTH-1:0] SRA, SRB,W_INST,R_INST, DR;   //source and destination reg
+	  input [DATA_WIDTH-1:0] WD, MEM_WD;            // write data
+	  output [DATA_WIDTH-1:0] RE_X,RE_A, RE_B;    //read data 
 	
 	
-	 reg [31:0] regfile [0:63];  //array of registers
+	reg [DATA_WIDTH-1:0] regfile [0:DEPTH];  //array of registers
 	initial
          begin
-          $readmemh("mem.txt",regfile,0,63);
+          $readmemh("mem.txt",regfile,0,DEPTH);
          end
 	 
 	 assign RE_A = regfile[SRA];
